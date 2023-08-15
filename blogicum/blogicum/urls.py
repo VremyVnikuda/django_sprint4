e402aa674bc73e84e2ff278b79d2e61ccfdd34ca
Антон <anton_kopnin@mail.ru>
2023-08-15 16:46:36 +0300

    Blogicum revision

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -7,6 +7,8 @@ handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.server_error'
 
 urlpatterns = [
+    path('pages/', include('pages.urls')),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
     path(
@@ -18,6 +20,4 @@ urlpatterns = [
         ),
         name='registration'
     ),
-    path('pages/', include('pages.urls')),
-    path('', include('blog.urls')),
 ]
