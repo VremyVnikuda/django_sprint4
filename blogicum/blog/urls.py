b82cf50582719c2a7ad64a93b870e58bdcf3ed75
Антон <anton_kopnin@mail.ru>
2023-08-17 00:10:46 +0300

    Blogicum revision

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -6,10 +6,8 @@ from django.conf import settings
 app_name = 'blog'
 
 urlpatterns = [
-     path('', views.IndexListView.as_view(),
-          name='index'),
-     path('posts/create/', views.PostCreateView.as_view(),
-          name='create_post'),
+     path('', views.IndexListView.as_view(), name='index'),
+     path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
      path('posts/<int:id>/', views.PostDetailView.as_view(),
           name='post_detail'),
      path('posts/<int:post_id>/edit/', views.EditPostView.as_view(),
@@ -18,15 +16,14 @@ urlpatterns = [
           name='delete_post'),
      path('category/<slug:category_slug>/',
           views.CategoryPostsListView.as_view(), name='category_posts'),
-     path('profile/<slug:username>/edit/', views.EditProfileListView.as_view(),
-          name='edit_profile'),
-     path('profile/<slug:username>/', views.ProfileListView.as_view(),
-          name='profile'),
-     path('posts/<int:post_id>/comment/', views.CommentCreateView.as_view(),
-          name='add_comment'),
+     path('profile/<slug:username>/edit/',
+          views.EditProfileListView.as_view(), name='edit_profile'),
+     path('profile/<slug:username>/',
+          views.ProfileListView.as_view(), name='profile'),
+     path('posts/<int:post_id>/comment/',
+          views.CommentCreateView.as_view(), name='add_comment'),
      path('posts/<int:post_id>/edit_comment/<comment_id>/',
           views.CommentUpdateView.as_view(), name='edit_comment'),
      path('posts/<int:post_id>/delete_comment/<int:comment_id>/',
           views.CommentDeleteView.as_view(), name='delete_comment'),
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+          ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
